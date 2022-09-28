2a9303927aae81a5a55f8fbe0cc529e306d5d3db
NamTuan Le <le_nam_tuan@artc.a-star.edu.sg>
2022-09-28 18:07:22 +0800

    enviroment port configuration update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 let app = express();
-
 app.use(function(req, res, next) {
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Origin', '*');
@@ -13,12 +11,10 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-
+//configure port
 var port = process.env.PORT || 4090;
 var ApiRouter = require('./v1/route');
-
 app.use('/v1', ApiRouter);
 app.listen(port);
-
 console.log('Server is running on port ' + port);
 
